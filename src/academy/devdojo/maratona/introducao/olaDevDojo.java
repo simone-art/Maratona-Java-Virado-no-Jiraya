6b80877dd3d27451bc827ab9cf811d4d00bc0324
simone-art <simoneperiodista3@gmail.com>
2023-11-06 18:16:08 -0300

    Comentários só se usam se for necessário

--- src/academy/devdojo/maratona/introducao/olaDevDojo.java
+++ src/academy/devdojo/maratona/introducao/olaDevDojo.java
@@ -1,6 +1,10 @@
 package academy.devdojo.maratona.introducao;
 
 public class olaDevDojo {
+    // Isso é um comentário em uma linha
+    /*
+    Isso é um comentário de múltiplas linhas
+     */
     public static void main (String[] args){
 
         System.out.println("Você iniciou a maratona devDojo");
