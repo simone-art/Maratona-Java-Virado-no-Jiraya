e3e11bc254f2457041d07125990d711573dbac92
simone-art <simoneperiodista3@gmail.com>
2023-11-08 17:04:38 -0300

    Operadores aritméticos Java

---
+++ src/academy/devdojo/maratona/introducao/AulaO4Operadores.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratona.introducao;
+
+public class AulaO4Operadores {
+
+    public static void main(String[] args) {
+
+        // Operadores básicos de matemática
+        int numero01 = 10;
+        int numero02 = 30;
+
+        int soma = numero01 + numero02;
+        int divisao = numero02 / numero01;
+        int multiplicacao = numero01 * numero02;
+        int subtracao = numero02 - numero01;
+
+        System.out.println(soma);
+        System.out.println(divisao);
+        System.out.println(multiplicacao);
+        System.out.println(subtracao);
+    }
+}
