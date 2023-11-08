1b7c4b43834130e956debb1ef4c9c53d692faa9c
simone-art <simoneperiodista3@gmail.com>
2023-11-08 15:32:42 -0300

    Tipos primitivos em Java

---
+++ src/academy/devdojo/maratona/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratona.introducao;
+
+public class Aula02TiposPrimitivos {
+
+    public static void main(String[] args) {
+
+        //Tipos primitivos guardam em memória um valor simples
+        // int, double, short, float, long, char, byte, boolean
+        int age = 10;
+        System.out.println("Age: " + age);
+
+    }
+}
