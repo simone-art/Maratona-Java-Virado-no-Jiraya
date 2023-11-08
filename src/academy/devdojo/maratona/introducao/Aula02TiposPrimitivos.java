0f3c953e4831b5f6763acdbf22ccfa9e4442463b
simone-art <simoneperiodista3@gmail.com>
2023-11-08 15:48:07 -0300

    Tipos primitivos em Java parte 2

--- src/academy/devdojo/maratona/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratona/introducao/Aula02TiposPrimitivos.java
@@ -8,6 +8,24 @@ public class Aula02TiposPrimitivos {
         // int, double, short, float, long, char, byte, boolean
         int age = 10;
         System.out.println("Age: " + age);
+        long bigNumber = 1000000;
+        System.out.println("long: " + bigNumber);
+        double salary = 2000;
+        System.out.println("double: " + salary);
+        char gender = 'F';
+        System.out.println("Char: " + gender);
+        float salaryFloat = 2500;
+        System.out.println("Float: " + salaryFloat);
+        byte ageByte = 10;
+        System.out.println("Byte: " + ageByte);
+        short ageShort = 10;
+        System.out.println("Short: " + ageShort);
+        boolean chooseItemAsTrue = true;
+        System.out.println("Boolean: " + chooseItemAsTrue);
+        boolean chooseItemAsFalse = false;
+        System.out.println("Boolean: " + chooseItemAsFalse);
+
+
 
     }
 }
